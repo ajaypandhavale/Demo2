53b8a765947d28dd9a9050d3ad63b08acb6af1b6
ajaypandhavale <aj40135821@gmail.com>
2025-02-05 21:17:45 +0530

    Revert "testing"
    This reverts commit 66d8f8ee

--- src/Demo.java
+++
@@ -1,9 +0,0 @@
-public class Demo {
-public static void main(String[] args)
-    {
-        System.out.println("Hii, demo");
-        System.out.println("testing");
-        System.out.println("testing");
-
-    }
-}
